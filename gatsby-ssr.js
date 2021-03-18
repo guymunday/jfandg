7c353137b56ef5315c7e50fda6bdda081a5eabc4
guymunday <guymunday@gmail.com>
2021-03-18 09:35:22 +0000

    gastby-ssr wrapRootElement

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,11 @@
 import * as React from "react"
 import Layout from "./src/components/layout"
+import { GlobalVideoProvider } from "./src/context/videoContext"
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <GlobalVideoProvider>{element}</GlobalVideoProvider>
 }
